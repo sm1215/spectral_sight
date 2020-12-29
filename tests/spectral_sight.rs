0577a292206618d977c9f783c57959de78e8a0bf
sm1215 <sm1215@gmail.com>
2020-12-29 13:49:50 -0500

    add testing setup / teardown

--- tests/spectral_sight.rs
+++ tests/spectral_sight.rs
@@ -1,6 +1,6 @@
 use spectral_sight::*;
-use std::fs;
 use std::path::Path;
+use std::{fs, io, panic};
 
 pub struct TestConfig {
     base_path: String,
@@ -16,28 +16,50 @@ impl TestConfig {
     }
 }
 
+// deletes entire backup directory, intended only for tearing down tests
+fn destroy_base_backup_folder(backup_path: &str) -> io::Result<()> {
+    fs::remove_dir_all(backup_path)?;
+    Ok(())
+}
+
+fn setup() {
+    let cfg = TestConfig::new();
+    let backup_path = [cfg.base_path, cfg.backup_location].join("//");
+    let _backup_result = create_base_backup_folder(&backup_path);
+}
+
+fn teardown() {
+    let cfg = TestConfig::new();
+    let backup_path = [cfg.base_path, cfg.backup_location].join("//");
+    let _backup_result = destroy_base_backup_folder(&backup_path);
+}
+
+fn run_test<T> (test: T) 
+where T: FnOnce() -> () + panic::UnwindSafe
+{
+    setup();
+    // wrapping the test in an enclosure will allow the program to error
+    // and still run the test teardown logic after
+    let result = panic::catch_unwind(|| {
+        test()
+    });
+    teardown();
+    assert!(result.is_ok())
+}
+
 #[test]
 fn creates_backup_folder() {
     let cfg = TestConfig::new();
     let backup_path = [cfg.base_path, cfg.backup_location].join("//");
-    let backup_result = create_base_backup_folder(&backup_path);
-
-    assert_eq!(backup_result, ());
+    let _backup_result = create_base_backup_folder(&backup_path);
     assert_eq!(Path::new(&backup_path).exists(), true);
 }
 
+#[test]
 fn destroys_backup_folder() {
-    
+    let cfg = TestConfig::new();
+    let backup_path = [cfg.base_path, cfg.backup_location].join("//");
+    let _backup_result = destroy_base_backup_folder(&backup_path);
+    assert_eq!(Path::new(&backup_path).exists(), false);
 }
 
-// #[test]
-// fn test_pick_folders() {
-//     let testing_path = "./tests/path_test";
-//     let testing_folders = [
-//         "include",
-//         "important",
-//     ];
-//     let result = pick_folders(&testing_path, &testing_folders);
-//     println!("result {:#?}", result);
-//     // assert_eq!("sample", output);
-// }
