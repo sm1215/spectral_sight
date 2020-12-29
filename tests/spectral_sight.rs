597769a106be895bddb41eddbf8444b51d5aed88
sm1215 <sm1215@gmail.com>
2020-12-29 11:52:48 -0500

    initial commit

---
+++ tests/spectral_sight.rs
@@ -0,0 +1,15 @@
+use spectral_sight::*;
+
+#[test]
+fn test_wow_path() {
+    set_wow_path();
+    // assert_eq!("sample", output);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
