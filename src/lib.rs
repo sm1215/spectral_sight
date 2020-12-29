ba47b966ce65795fb370fee1078b21da482493e9
sm1215 <sm1215@gmail.com>
2020-12-29 13:16:54 -0500

    pick_folders ? wip, create backup location, tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
 use std::path::{Path};
 use std::{fs, io};
 
-pub fn read_contents(target_path: String) -> io::Result<()> {
-    println!("read_contents running...");
-    let default_path = String::from("./");
-    let wow_path = Path::new(&target_path);
+pub fn create_base_backup_folder(backup_path: &str) -> io::Result<()> {
+    fs::create_dir(backup_path)?;
+    Ok(())
+}
+
+pub fn pick_folders(target_path: &str, target_folders: &[&str]) -> io::Result<()> {
+    println!("pick_folders running...");
+    let wow_path = Path::new(target_path);
 
     let mut entries = fs::read_dir(wow_path)?
-        .map(|res| res.map(|e| e.path()))
+        .map(|res| {
+            // println!("res {:#?}", res);
+            res.map(|e| e.path())
+        })
         .collect::<Result<Vec<_>, io::Error>>()?;
 
     println!("entries {:#?}", entries);
--- tests/spectral_sight.rs
+++ tests/spectral_sight.rs
@@ -1,9 +1,43 @@
 use spectral_sight::*;
+use std::fs;
+use std::path::Path;
+
+pub struct TestConfig {
+    base_path: String,
+    backup_location: String,
+}
+
+impl TestConfig {
+    pub fn new() -> Self {
+        Self {
+            base_path: String::from("./tests/path_test"),
+            backup_location: String::from("interface_backups"),
+        }
+    }
+}
 
 #[test]
-fn test_read_contents() {
-    let testing_path = String::from("./tests/path_test");
-    let result = read_contents(testing_path);
-    println!("result {:#?}", result);
-    // assert_eq!("sample", output);
+fn creates_backup_folder() {
+    let cfg = TestConfig::new();
+    let backup_path = [cfg.base_path, cfg.backup_location].join("//");
+    let backup_result = create_base_backup_folder(&backup_path);
+
+    assert_eq!(backup_result, ());
+    assert_eq!(Path::new(&backup_path).exists(), true);
 }
+
+fn destroys_backup_folder() {
+    
+}
+
+// #[test]
+// fn test_pick_folders() {
+//     let testing_path = "./tests/path_test";
+//     let testing_folders = [
+//         "include",
+//         "important",
+//     ];
+//     let result = pick_folders(&testing_path, &testing_folders);
+//     println!("result {:#?}", result);
+//     // assert_eq!("sample", output);
+// }
