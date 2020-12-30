ce060b100feb01f1dba66478ff2c10609db935d8
sm1215 <sm1215@gmail.com>
2020-12-30 09:15:31 -0500

    pushing latest work

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
-use std::path::{Path};
+// use std::path::{Path};
 use std::{fs, io};
 
 pub fn create_base_backup_folder(backup_path: &str) -> io::Result<()> {
     fs::create_dir(backup_path)?;
+    // set_perms(backup_path);
     Ok(())
+}
+
+pub fn copy_folder(source_path: &str, destination_path: &str) -> io::Result<()> {
+    println!("copy source {:?}", source_path);
+    println!("copy destination {:?}", destination_path);
+
+    set_perms(source_path);
+    set_perms(destination_path);
+    fs::copy(source_path, destination_path)?;
+    Ok(())
+}
+
+pub fn set_perms(path: &str) {
+    let mut perms = fs::metadata(&path)
+        .expect("error getting permissions in test setup")
+        .permissions();
+    perms.set_readonly(false);
+    fs::set_permissions(&path, perms.clone())
+        .expect("error setting permissions in test setup");
+    println!("perms {:#?}", &perms);
 }
--- tests/spectral_sight.rs
+++ tests/spectral_sight.rs
@@ -10,9 +10,13 @@ pub struct TestConfig {
 impl TestConfig {
     pub fn new() -> Self {
         Self {
-            base_path: String::from("./tests/path_test"),
+            base_path: String::from("./tests/source_test"),
             backup_location: String::from("interface_backups"),
         }
+    }
+    pub fn get_backup_path() -> String {
+        let cfg = Self::new();
+        [cfg.base_path, cfg.backup_location].join("/")
     }
 }
 
@@ -22,16 +26,44 @@ fn destroy_base_backup_folder(backup_path: &str) -> io::Result<()> {
     Ok(())
 }
 
+fn set_perms(path: &str) {
+    let path = String::from("./tests");
+    let mut perms = fs::metadata(&path)
+        .expect("error getting permissions in test setup")
+        .permissions();
+    perms.set_readonly(false);
+    fs::set_permissions(&path, perms)
+        .expect("error setting permissions in test setup");
+}
+
 fn setup() {
-    let cfg = TestConfig::new();
-    let backup_path = [cfg.base_path, cfg.backup_location].join("//");
-    let _backup_result = create_base_backup_folder(&backup_path);
+    println!("\n running test setup...");
+    let backup_path = &TestConfig::get_backup_path();
+    let _create_backup_result = create_base_backup_folder(backup_path);
+    println!("backup directory created at {:?}", backup_path);
+
+    // directory is initially created with read-only permissions
+    let mut perms = fs::metadata(backup_path)
+        .expect("error getting permissions in test setup")
+        .permissions();
+    perms.set_readonly(false);
+    fs::set_permissions(backup_path, perms.clone())
+        .expect("error setting permissions in test setup");
+    
+        println!("backup directory perms {:#?}", perms);
+
+    let backup_path_exists = Path::new(&backup_path).exists();
+    assert_eq!(backup_path_exists, true);
 }
 
 fn teardown() {
-    let cfg = TestConfig::new();
-    let backup_path = [cfg.base_path, cfg.backup_location].join("//");
-    let _backup_result = destroy_base_backup_folder(&backup_path);
+    println!("running test teardown...");
+    let backup_path = &TestConfig::get_backup_path();
+    let _destry_backup_result = destroy_base_backup_folder(&TestConfig::get_backup_path());
+    
+    let backup_path_exists = Path::new(&backup_path).exists();
+    println!("backup directory removed {:?}", backup_path_exists);
+    assert_eq!(backup_path_exists, false);
 }
 
 fn run_test<T> (test: T) 
@@ -43,23 +75,44 @@ where T: FnOnce() -> () + panic::UnwindSafe
     let result = panic::catch_unwind(|| {
         test()
     });
-    teardown();
+    println!("test result {:#?}", result);
+    // teardown();
     assert!(result.is_ok())
 }
 
-#[test]
-fn creates_backup_folder() {
-    let cfg = TestConfig::new();
-    let backup_path = [cfg.base_path, cfg.backup_location].join("//");
-    let _backup_result = create_base_backup_folder(&backup_path);
-    assert_eq!(Path::new(&backup_path).exists(), true);
-}
+// #[test]
+// fn creates_backup_folder() {
+//     let backup_path = &TestConfig::get_backup_path();
+//     println!("backup_path {:?}", backup_path);
+//     let _backup_result = create_base_backup_folder(backup_path);
+//     assert_eq!(Path::new(&backup_path).exists(), true);
+// }
+
+// #[test]
+// fn destroys_backup_folder() {
+//     let backup_path = &TestConfig::get_backup_path();
+//     let _backup_result = destroy_base_backup_folder(&backup_path);
+//     assert_eq!(Path::new(backup_path).exists(), false);
+// }
 
 #[test]
-fn destroys_backup_folder() {
+fn copies_a_folder() {
     let cfg = TestConfig::new();
-    let backup_path = [cfg.base_path, cfg.backup_location].join("//");
-    let _backup_result = destroy_base_backup_folder(&backup_path);
-    assert_eq!(Path::new(&backup_path).exists(), false);
-}
+    let base_path = cfg.base_path;
+    let backup_location = cfg.backup_location;
+    let target_dir = String::from("include");
+    let target_file = String::from("payload.txt");
 
+    let source = [base_path.clone(), target_dir.clone(), target_file.clone()].join("/");
+    let destination = [base_path.clone(), backup_location.clone(), target_file.clone()].join("/");
+
+    run_test(|| {
+        let _copy_result = copy_folder(&source, &destination);
+        println!("copy result {:#?}", _copy_result);
+
+        let backed_up_file = [destination.clone(), target_dir.clone()].join("/");
+        println!("backup path {:?}", backed_up_file);
+
+        assert_eq!(Path::new(&backed_up_file).exists(), true);
+    })
+}
